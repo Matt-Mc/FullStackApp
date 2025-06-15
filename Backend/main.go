0856aff8995875a1b9124bc499c48e8e151f48bb
Matt McCarty <mcc.15.matt@gmail.com>
2025-06-14 20:17:36 -0400

    Added parsing for MP's, fixed Goroutines

--- Backend/main.go
+++ Backend/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
+	"sync"
 )
 
 type BillName struct {
@@ -26,16 +26,35 @@ type BillResponse struct {
 	Objects []Bill `json:"objects"`
 }
 
-type MP struct {
-	ID           string `json:"id"`
-	Name         string `json:"name"`
-	Constituency string `json:"constituency"`
-	Party        string `json:"party"`
+type MPResponse struct {
+	Objects []MP `json:"objects"`
 }
 
-func getBills() {
+type MP struct {
+	Name         string `json:"name"`
+	URL          string `json:"url"`
+	CurrentParty Party `json:"current_party"`
+	CurrentRiding Riding `json:"current_riding"`
+	Image        string   `json:"image"`
+}
+
+type Riding struct {
+	Province string `json:"province"`
+	Name     struct {
+		EN string `json:"en"`
+	} `json:"name"`
+}
+
+type Party struct {
+	ShortName struct {
+		EN string `json:"en"`
+	} `json:"short_name"`
+}
+
+func getBills() error {
 	resp, err := http.Get("https://api.openparliament.ca/bills/?format=json")
 	if err != nil {
+		return fmt.Errorf("failed to make HTTP request for Bills: %w", err)
 		print(err)
 	}
 	defer resp.Body.Close()
@@ -45,11 +64,11 @@ func getBills() {
 
 	err = json.Unmarshal(body, &billResponse)
 	if err != nil {
-		print(err)
+		return fmt.Errorf("failed to read response body for bills: %w", err)
 	}
 	if len(billResponse.Objects) == 0 {
-		print("No bills found in response")
-		return
+		fmt.Println("No bills found in response.")
+		return nil
 	}
 
 	fmt.Printf("successfully got %d bills\n", len(billResponse.Objects))
@@ -60,30 +79,69 @@ func getBills() {
 		fmt.Printf("Bill %d: %s\n", i, bill.Introduced)
 		fmt.Printf("Bill %d: %s\n", i, bill.Url)
 	}
+
+	return nil
 }
 
-func getMPs() {
+func getMPs() error {
 	resp, err := http.Get("https://api.openparliament.ca/politicians/?format=json")
 	if err != nil {
-		print(err)
+		return fmt.Errorf("failed to make HTTP request for MPs: %w", err)
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	var mps []MP
+
+	var mps MPResponse
+
 	err = json.Unmarshal(body, &mps)
 	if err != nil {
-		print(err)
+		return fmt.Errorf("failed to read response body for MPs: %w", err)
+
 	}
+	if len(mps.Objects) == 0 {
+		fmt.Println("No MPs found in response.")
+		return nil
+	}
+	fmt.Printf("successfully got %d MPs\n", len(mps.Objects))
+	for i, mp := range mps.Objects {
+		fmt.Printf("MP %d: %s\n", i, mp.Name)
+		fmt.Printf("MP %d: %s\n", i, mp.CurrentParty.ShortName.EN)
+		fmt.Printf("MP %d: %s\n", i, mp.CurrentRiding.Name.EN)
+		fmt.Printf("MP %d: %s\n", i, mp.CurrentRiding.Province)
+		fmt.Printf("MP %d: %s\n", i, mp.Image)
+	}
+	return nil
 }
 
-func getData() {
-	go getBills()
-	//go getMPs()
+func getData(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := getBills(); err != nil {
+			fmt.Printf("Error getting bills: %v\n", err)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := getMPs(); err != nil {
+			fmt.Printf("Error getting MPs: %v\n", err)
+		}
+	}()
 
 }
 
 func main() { // This is the main function, the entry point of your program.
-	print("Starting Go Routines")
-	go getData()
-	time.Sleep(time.Second * 10)
+	fmt.Println("Starting Go Routines")
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go getData(&wg)
+
+	wg.Wait()
+	fmt.Println("All Go Routines completed")
 }
